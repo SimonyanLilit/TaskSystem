97df805b052dd0e89697bc327fe6b3992feebdce
Lilit Simonyan <lilitsimonyan103@gmail.com>
2022-12-04 16:00:29 +0400

    First commit

---
+++ Lesson1/src/Odds.java
@@ -0,0 +1,18 @@
+public class Odds {
+    public static void main(String[] args) {
+
+int[] array=new int[41];
+        for (int i = -19,j=0; i <21 ; i+=2,j++)
+
+    {
+array[j]=i;
+        System.out.print(array[j]+" ");
+    }
+
+            }
+        }
+
+
+
+
+
